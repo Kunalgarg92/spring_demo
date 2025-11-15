6000cd3b89be2ae7231f41ebae474e41808b4c70
Kunalgarg92 <kunal.garg2022@vitstudent.ac.in>
2025-11-16 00:53:45 +0530

    update for one-to-one , validations and many-to-many

--- src/main/java/com/chubb/request/Order1.java
+++ src/main/java/com/chubb/request/Order1.java
@@ -1,11 +1,21 @@
 package com.chubb.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+import java.util.Set;
 
+import com.chubb.request.Address;
+
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
@@ -23,6 +33,17 @@ public class Order1 {
 	private float price;
 	private int quantity;
 	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private List<Address> addresses;
+	
+	  @ManyToMany(cascade = {CascadeType.MERGE})
+	    @JoinTable(
+	        name = "order_shared_address",
+	        joinColumns = @JoinColumn(name = "order_id"),
+	        inverseJoinColumns = @JoinColumn(name = "address_id")
+	    )
+	    private Set<Address> sharedAddresses;
+
 //	private Address address;
 //	public int getQuantity() {
 //		return quantity;
--- src/main/java/com/chubb/service/OrderService.java
+++ src/main/java/com/chubb/service/OrderService.java
@@ -1,21 +1,42 @@
 package com.chubb.service;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.chubb.repository.AddressRepository;
 import com.chubb.repository.OrderRepository;
+import com.chubb.request.Address;
 import com.chubb.request.Order1;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Service
-@Slf4j
 public class OrderService {
-	@Autowired
-	OrderRepository orderRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private AddressRepository addressRepository;
+
     public void inserOrder(Order1 order) {
-        log.debug(order.toString());
+        if (order.getSharedAddresses() != null) {
+            Set<Address> attached = new HashSet<>();
+            for (Address a : order.getSharedAddresses()) {
+                if (a.getId() != 0) {
+                    Address existing = addressRepository.findById(a.getId())
+                            .orElseThrow(() -> new RuntimeException("Address not found: " + a.getId()));
+                    attached.add(existing);
+                } else {
+                    Address saved = addressRepository.save(a);
+                    attached.add(saved);
+                }
+            }
+            order.setSharedAddresses(attached);
+        }
         orderRepository.save(order);
-        System.out.println(order);
     }
 }
